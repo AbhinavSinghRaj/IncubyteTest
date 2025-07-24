b3c3cdfd39b438b5661cb4cf48d90266a680fdc6
AbhinavSinghRaj <abhinavraajput@gmail.com>
2025-07-24 13:05:11 +0530

    support newlines as valid delimiters

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -16,3 +16,6 @@ it('should return the sum of two numbers', () => {
 it('should handle any amount of numbers', () => {
     expect(add("1,2,3,4")).toBe(10);
   });
+it('should support newlines between numbers', () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,5 +1,5 @@
 export function add(numbers: string): number {
     if (numbers === "") return 0;
-    const parts = numbers.split(",");
+    const parts = numbers.split(/,|\n/);
     return parts.map(Number).reduce((sum, n) => sum + n, 0);
 }
