51d48b54bcd0813f568ad1ab68f1096fd3a55f0c
AbhinavSinghRaj <abhinavraajput@gmail.com>
2025-07-24 13:01:02 +0530

    return sum of two comma-separated numbers

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,11 +1,15 @@
 import { add } from './calculator';
 
 describe('String Calculator', () => {
-  it('should return 0 for an empty string', () => {
-    expect(add("")).toBe(0);
-  });
+    it('should return 0 for an empty string', () => {
+        expect(add("")).toBe(0);
+    });
 });
 
 it('should return the number when only one number is passed', () => {
     expect(add("5")).toBe(5);
-  });
+});
+
+it('should return the sum of two numbers', () => {
+    expect(add("1,2")).toBe(3);
+});
